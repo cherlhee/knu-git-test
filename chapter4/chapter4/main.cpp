f54182abee1873404cc1985f4a29d170c2fcadb6
cherlhee <cherlhee@jcradar.com>
2025-03-26 22:03:15 +0900

    modified

--- chapter4/chapter4/main.cpp
+++ chapter4/chapter4/main.cpp
@@ -4,15 +4,22 @@ using namespace std;
 
 int main() {
 
-	int* p;
+	int* p = new int;
+	cout << p << endl;
+
 
 	int a = 100;
 
 
-	p = new int;
+
+
 
 	*p = 5;
+	cout << *p << endl;
 
+
+	int temp = *p;
+	cout << temp << endl;
 
 
 	cout << "hello,kitty";
